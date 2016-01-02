f0d39ea680304261d0b1a5a062c1a7a60b803faf
Daniel Lwo <danielxji6@gmail.com>
2016-01-01 20:53:38 -0800

    finish 1 to 4 test

--- drills.rb
+++ drills.rb
@@ -1,158 +1,191 @@
 #########################
 #### USING ARGUMENTS ####
 #########################
-#say_hello
-  # returns 'hello'
+def say_hello
+  'hello'
+end
 
-#echo
-  # returns the input string
+def echo(string)
+  string
+end
 
-#eddie_izzards_height
-  # calculates Eddie Izzards height (67in)
-  # and takes into account the height of his heel (default: 0in)
+def eddie_izzards_height(heel_height = 0)
+  67 + heel_height
+end
 
-#how_many_args
-  # accepts any number of arguments without complaint
-  # returns the total number of arguments passed to the function
+def how_many_args(*arg)
+  arg.count
+end
 
-#find_answer
-  # returns the value associated with the 'answer' keyword argument
-  # returns nil if it cannot find the 'answer' keyword argument
-  # complains when given non-keyword arguments
+def find_answer(hash = {})
+  hash[:answer]
+end
 
 ##############################
 #### MANIPULATING STRINGS ####
 ##############################
-#first_char
-  # returns the first letter of the word
-  # lowercases the first letter of the word
+def first_char(word)
+  word[0].downcase
+end
 
-#polly_wanna
-  # echoes the original word
-  # repeats the original word 3 times
+def polly_wanna(word)
+  word
+  word + word + word
+end
 
-#count_chars
-  # returns the number of characters in a string
+def count_chars(word)
+  word.size
+end
 
-#yell
-  # convert the message to uppercase
-  # adds an exclamation point to the end of the message
+def yell(string)
+  string.upcase + "!"
+end
 
-#to_telegram
-  # replaces periods with ' STOP'
+def to_telegram(string)
+  string.gsub(".", " STOP")
+end
 
-#spell_out
-  # returns the input string, with characters seperated by dashes
-  # converts the string to lowercase
+def spell_out(word)
+  word.downcase.split("").join("-")
+end
 
-#seperate
-  # seperates characters with a custom seperator, when supplied with one
-  # seperates characters with dashes (by default)
+def seperate(word, sep = "-")
+  word.downcase.split("").join(sep)
+end
 
-#croon
-  # seperates word characters with dashes
-  # preserves whitespace between words
+def croon(string)
+  string
+  .split(" ")
+  .map {|n| n.split("").join("-")}
+  .join(" ")
+end
 
-#palindrome_word?
-  # determines whether a single word is a palindrome
-  # ignores case
+def palindrome_word?(word)
+  re_word = word.downcase
+  re_word == re_word.reverse
+end
 
-#palindrome_sentence?
-  # determines whether a sentence is a palindrome
-  # ignores case
-  # ignores whitespace
-  # ignores punctuation
+def palindrome_sentence?(string)
+  re_string = string.downcase.gsub(/\W/, "")
+  re_string == re_string.reverse
+end
 
-#is_vowel
-  # determines whether a given character is a vowel
-  # ignores case
-  # handles weird inputs gracefully
+def is_vowel(char)
+  if char.is_a?(String)
+    char.downcase.count('aeiou') > 0
+  else
+    false
+  end
+end
 
-#add_period
-  # adds a period to the end of the sentence
-  # does not add a period if one is already there
-  # does not add a period if any form of terminal punctuation is present
+def add_period(string)
+  string[-1].match(/\W/) != nil ? string : string + "."
+end
 
 
 ###########################
 #### LOOPS & ITERATORS ####
 ###########################
-#count_spaces
-  # counts the spaces in a string
+def count_spaces(string)
+  string.count(" ")
+end
 
-#string_lengths
-  # converts a list of strings to a list of string lengths
+def string_lengths(ary)
+  ary.map! {|n| n = n.size}
+end
 
-#remove_falsy_values
-  # filters out falsy values from a list
+def remove_falsy_values(ary)
+  ary.delete_if {|n| !n == true}
+end
 
-#exclude_last
-  # removes the last item from an array
-  # removes the last item from a string
+def exclude_last(arg)
+  arg[0..-2]
+end
 
-#exclude_first
-  # removes the first item from an array
-  # removes the first character from a string
-  # does not alter the original input (non-destructive)
+def exclude_first(arg)
+  arg[1..-1]
+end
 
-#exclude_ends
-  # removes the first and last items from an array
-  # removes the first and last characters from a string
+def exclude_ends(arg)
+  arg[1..-2]
+end
 
-#select_every_even
-  # returns a list of even-indexed items
+def select_every_even(ary)
+  ary.delete_if.with_index {|ele, i| i % 2 == 1}
+end
 
-#select_every_odd
-  # returns a list of odd-indexed items
+def select_every_odd(ary)
+  ary.delete_if.with_index {|ele, i| i % 2 == 0}
+end
 
-#select_every_n
-  # returns a list of items at an index evenly divisible by n
-  # defaults to an n value of 1
+def select_every_n(ary, n = 1)
+  ary.delete_if.with_index {|ele, i| i % n != 0}
+end
 
-#compile_agenda
-  # converts a list of agenda items into a single string
-  # titles start with a bullet ("*") and are separated by line breaks ("/n")
-  # sorts items by priority ascending (low to high) by default
-  # sort order can (optionally) be changed to priority descending
-  # the bullet can (optionally) be changed to any symbol
+def compile_agenda(*agenda)
+  puts agenda
+  agenda[2] = agenda[2] || "*"
+  result = ""
+  agenda[0].sort! {|x, y| x[:priority] <=> y[:priority]}
+  agenda[0].sort! {|x, y| y[:priority] <=> x[:priority]} if agenda[1] == "ASC"
+  agenda[0].each do |ele|
+    result += "#{agenda[2]} " + ele[:title] + "\n"
+  end
+  result.chop
+end
 
 ##############################
 #### MANIPULATING NUMBERS ####
 ##############################
-#count_to
-  # returns an array containing every integer from 0 to n
-  # rounds off decimals
+def count_to(n)
+  ary = [0]
+  n.floor.times {|i| ary.push(i + 1)} if n > 0
+  n.abs.floor.times {|i| ary.push(-1 - i)} if n < 0
+  ary
+end
 
-#is_integer?
-  # returns true for Fixnums and Bignums (whole number or 'integer' types)
-  # returns true for Floats (decimals) equal to integers
-  # returns false for non-integer decimals
-  # returns false for Float::NAN
-  # returns false for non-numbers
+def is_integer?(num)
+  num.kind_of?(Integer) ||
+  (num.is_a?(Float)  && !num.nan? && num == num.to_i)
+end
 
-#is_prime?
-  # returns false for non-integer decimals
-  # returns false for numbers less than or equal to 1
-  # returns false for numbers divisible by anything but 1 and themselves
-  # returns true for prime numbers
+def is_prime?(num)
+  if num.is_a?(Integer) && num > 0
+    (Math.sqrt(num).floor - 1).times do |i|
+      return false if num % (i + 2) == 0
+    end
+    return true
+  end
+  return false
+end
 
-#primes_less_than
-  # returns an empty array if there are no primes below num
-  # does not return the number itself
-  # finds all primes less than the given number
+def primes_less_than(n)
+  ary = []
+  (2..n-1).each do |i|
+    ary.push(i) if is_prime?(i)
+  end
+  ary
+end
 
-#iterative_factorial
-  # returns 1 for 0 and 1
-  # returns NaN for numbers less than 0
-  # returns NaN for non-integers
-  # calculates factorial
+def iterative_factorial(n)
+  if n < 0 || !n.is_a?(Integer)
+    return Float::NAN
+  else
+    sum = 1
+    (1..n).each {|i| sum *= i}
+    return sum
+  end
+end
 
 
 
 ##############################
 #### MANIPULATING OBJECTS ####
 ##############################
-#character_count
+def character_count(word)
+
+
+end
   # counts how many times each character appears in a string
   # ignores case
 
